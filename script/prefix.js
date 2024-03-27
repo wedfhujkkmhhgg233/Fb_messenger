9fccd8cf91fb194611c73c7a38c08b28ed272119
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2024-03-27 12:01:37 +0000

    Pwede naren

--- script/prefix.js
+++ script/prefix.js
@@ -27,13 +27,13 @@ module.exports.run = function ({ api, event, prefix, admin }) {
 	api.sendMessage(
 		{
 			body: `Yo, my prefix is [ 𓆩 ${prefix} 𓆪 ]\n\n𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:\n➥ ${prefix}help [number of page] -> see commands\n➥ ${prefix}sim [message] -> talk to bot\n➥ ${prefix}callad [message] -> report any problem encountered\n➥ ${prefix}help [command] -> information and usage of command\n\nHave fun using it, enjoy! ❤️\nBot Developer: https://www.facebook.com/${admin}`,
-			attachment: fs.createReadStream(__dirname + `./cache/prefix.gif`),
+			attachment: fs.createReadStream(__dirname + `/cache2/prefix.gif`),
 		},
 		threadID,
 		messageID
 	);
 
-	const voiceFile = fs.readFileSync(__dirname + "./cache/cliff.mp3");
+	const voiceFile = fs.readFileSync(__dirname + "/cache/cliff.mp3");
 	api.sendMessage(
 		{
 			attachment: voiceFile,
